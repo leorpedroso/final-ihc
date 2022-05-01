2c504c36b3e03f8739e4d9337886cae1ca1c24c6
leorpedroso <leorpedroso@hotmail.com>
2022-05-01 20:13:14 -0300

    checkin

--- app/src/main/java/com/example/trabfinal/MainActivity.java
+++ app/src/main/java/com/example/trabfinal/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.trabfinal.login.RegisterActivity;
 import com.example.trabfinal.reservecourt.SelectCourtTypeActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.Locale;
 
@@ -40,9 +44,7 @@ public class MainActivity extends AppCompatActivity {
         buttonAgendar.setOnClickListener(this::send);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setNegativeButton("Fechar", (dialog, id) -> {
-            dialog.dismiss();
-        });
+        builder.setNegativeButton("Fechar", (dialog, id) -> dialog.dismiss());
 
         dialog = builder.create();
         dialog.setView(mapImage);
@@ -50,8 +52,25 @@ public class MainActivity extends AppCompatActivity {
         Button buttonMap = findViewById(R.id.buttonMapa);
         buttonMap.setOnClickListener(this::show_dialog);
 
+        Button buttonCamera = findViewById(R.id.buttonCheckin);
+        buttonCamera.setOnClickListener(view -> {
+            IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+            intentIntegrator.initiateScan();
+        });
+
         TextView signout = findViewById(R.id.textDeslogar);
         signout.setOnClickListener(this::send_signout);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable android.content.Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            // If result was successfull
+            Toast.makeText(getApplicationContext(), "Checkin realizado com sucesso!", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void send(View view) {
