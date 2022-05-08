3454e0985012f622f469ccbe2610b2025009b807
leorpedroso <leorpedroso@hotmail.com>
2022-05-08 11:23:33 -0300

    arrumar checkin

--- app/src/main/java/com/example/trabfinal/MainActivity.java
+++ app/src/main/java/com/example/trabfinal/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.trabfinal.backend.Data;
+import com.example.trabfinal.backend.Reservation;
 import com.example.trabfinal.login.RegisterActivity;
 import com.example.trabfinal.reservecourt.SelectCourtTypeActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -79,9 +80,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable android.content.Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            // If result was successfull
-            Toast.makeText(getApplicationContext(), "Checkin realizado com sucesso!", Toast.LENGTH_LONG).show();
+        try {
+            if (result != null && !result.getContents().isEmpty()) {
+                // If result was successfull
+                Toast.makeText(getApplicationContext(), "Checkin realizado com sucesso!", Toast.LENGTH_LONG).show();
+                for (Reservation r : Data.getReservations()) {
+                    if (!r.getCheckInStatus()) {
+                        r.setCheckIn(true);
+                        break;
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Falha ao realizar check in!", Toast.LENGTH_LONG).show();
         }
     }
 
